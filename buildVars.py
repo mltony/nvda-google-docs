f78b41c55175af93111f9ca528b560d7a2ab4a81
mltony <anton.malykh+0@gmail.com>
2024-01-05 16:35:15 -0800

    Bump version to 1.0

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Accessibility enhancements for Google Docs."""),
 	# version
-	"addon_version": "0.1dev",
+	"addon_version": "1.0",
 	# Author(s)
 	"addon_author": "Tony Malykh <anton.malykh@gmail.com>",
 	# URL for the add-on documentation support
